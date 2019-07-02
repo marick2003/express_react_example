3f8f091d493e9ede672a0777c4e8c18a508e7a77
marick <mmaarriicckk@gmail.com>
2019-07-02 16:50:53 +0800

    add socket demo
    test socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,99 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import 
+
+// class App extends Component {
+// state = {
+//     data: null
+//   };
+
+//   componentDidMount() {
+//       // Call our fetch function below once the component mounts
+//     this.callBackendAPI()
+//       .then(res => this.setState({ data: res.express }))
+//       .catch(err => console.log(err));
+//   }
+//     // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+//   callBackendAPI = async () => {
+//     const response = await fetch('/express_backend');
+//     const body = await response.json();
+
+//     if (response.status !== 200) {
+//       throw Error(body.message) 
+//     }
+//     return body;
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         // Render the newly fetched data inside of this.state.data 
+//         <p className="App-intro">{this.state.data}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+// Updated. Thanks to: Paul Luna
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
-state = {
-    data: null
-  };
+  constructor() {
+    super();
+    this.state = {
+      endpoint: "localhost:5000",
 
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+      ///
+      color: 'white'
+      ///
+
+    };
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
+  // sending sockets
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('change color', this.state.color) // change 'red' to this.state.color
+  }
+  ///
+
+  // adding the function
+  setColor = (color) => {
+    this.setState({ color })
+  }
+
+  componentDidMount = () => {
+      const socket = socketIOClient(this.state.endpoint);
+      setInterval(this.send(), 1000)
+      socket.on('change color', (col) => {
+          document.body.style.backgroundColor = col
+      })
+  }
 
   render() {
+    // testing for socket connections
+
+    const socket = socketIOClient(this.state.endpoint);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data 
-        <p className="App-intro">{this.state.data}</p>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send() }>Change Color</button>
+
+
+
+        <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button id="red" onClick={() => this.setColor('red')}>Red</button>
+
       </div>
-    );
+    )
   }
 }
-
 export default App;
